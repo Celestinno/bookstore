a1a3c27eb0be2f5d543a922b8b176a24403ce9a4
Celestinno <celestino_gabriel@hotmail.com>
2022-06-15 14:46:22 -0300

    polished up design, main body and started on navigation buttons

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,17 +6,18 @@ export default function Body({ data }) {
     <div className='mainBody'>
       <div className='booksContainer'>
         {data.map((book) => (
+
           <div className='eachBook'>
             <img className='cover' src={book.cover}></img>
             <div className='info'>
-              <p className='title'>{book.name}</p>
+              <p className='title'>{book.name.substring(0, 40)}</p>
               <p className='author'>{book.author}</p>
             </div>
-
-
           </div>
         ))}
+
       </div>
+      <div>buttons</div>
     </div>
   )
 }
